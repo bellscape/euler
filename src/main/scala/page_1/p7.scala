27a4512c17fdba5272fad8092bc445109fd75643
bell <bellscape@gmail.com>
2017-04-11 12:01:33 +0800

    p8

---
+++ src/main/scala/page_1/p7.scala
@@ -0,0 +1,14 @@
+package page_1
+
+object p7 {
+
+	// ref: http://stackoverflow.com/q/15594227
+	val primes: Stream[Int] = 2 #:: Stream.from(3).filter(i =>
+		primes.takeWhile(p => p * p <= i).forall(p => i % p != 0))
+
+	def main(args: Array[String]): Unit = {
+		println(primes.apply(6 - 1))
+		println(primes.apply(10001 - 1))
+	}
+
+}
